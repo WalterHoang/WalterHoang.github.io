01a8fa4f7893f612519646b76a91fb5c8805a0ad
Walter Hoang <whoang@catalyte.io>
2019-11-21 10:43:21 -0700

    modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'contact', component: ContactInfoComponent },
+  { path: 'projects', component: ProjectsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { SkillListComponent } from './home/skill-list/skill-list.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectListComponent } from './projects/project-list/project-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    SkillListComponent,
+    ContactInfoComponent,
+    ProjectsComponent,
+    ProjectListComponent,
   ],
   imports: [
     BrowserModule,
